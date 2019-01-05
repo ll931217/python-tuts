e084be7a5f5d2140fd753581eb3bc31714fca030
Liang-Shih Lin <liangshihlin@yahoo.com>
2019-01-05 17:37:44 +0800

    First Commit

---
+++ homework1.py
@@ -0,0 +1,25 @@
+"""
+Author: Liang-Shih Lin
+Description: A file containing variables representing my favourite song.
+"""
+
+# Details of the song
+Name = "What A Catch, Donnie"
+Artist = "Fall Out Boy"
+Album = "Flie a Deux"
+Duration = (4 * 60) + 51
+WrittenBy = [
+    "Andrew Hurley",
+    "Joe Trohman",
+    "Patrick Stump",
+    "Pete Wentz"
+]
+ProducedBy = "Neal Avron"
+
+# Displaying of the details
+print(Name)
+print(Artist)
+print(Album)
+print(Duration)
+print(WrittenBy)
+print(ProducedBy)
